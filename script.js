5692fca6f58e568da8222be1f5952729c305ae51
sheymantech <olaniyansheyi1704@gmail.com>
2024-01-12 21:23:07 +0100

    New feature

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 const vm = Vue.createApp({
-  data() {
-    return {
-      firstName: "sheyman",
-      lastName: "olaniyan",
-      middleName: "",
-      url: "https://google.com",
-      raw_url: `<a href="https://google.com">google link</a>`,
-      age: 20,
-    };
-  },
-  methods: {
-    // fullName() {
-    //   console.log("fullNmae was called");
-    //   return `${this.lastName} ${
-    //     this.middleName
-    //   } ${this.firstName.toUpperCase()}`;
-    // },
-    increament() {
-      this.age++;
-    },
-    updateLastName(msg, event) {
-      // event.preventDefault();
-      console.log(msg);
-      this.lastName = event.target.value;
-    },
-    updateMiddleName(event) {
-      this.middleName = event.target.value;
-    },
-  },
-  computed: {
-    fullName() {
-      // this.age;
-      console.log("fullName computed was called");
-      return `${this.lastName} ${
-        this.middleName
-      } ${this.firstName.toUpperCase()}`;
-    },
-  },
-  watch: {
-    age(newVal, oldVal) {
-      setTimeout(() => {
-        this.age = 20;
-      }, 2000);
-    },
-  },
+  // data() {
+  //   return {
+  //     firstName: "sheyman",
+  //     lastName: "olaniyan",
+  //     middleName: "",
+  //     url: "https://google.com",
+  //     raw_url: `<a href="https://google.com">google link</a>`,
+  //     age: 20,
+  //   };
+  // },
+  // methods: {
+  //   // fullName() {
+  //   //   console.log("fullNmae was called");
+  //   //   return `${this.lastName} ${
+  //   //     this.middleName
+  //   //   } ${this.firstName.toUpperCase()}`;
+  //   // },
+  //   increament() {
+  //     this.age++;
+  //   },
+  //   updateLastName(msg, event) {
+  //     // event.preventDefault();
+  //     console.log(msg);
+  //     this.lastName = event.target.value;
+  //   },
+  //   updateMiddleName(event) {
+  //     this.middleName = event.target.value;
+  //   },
+  // },
+  // computed: {
+  //   fullName() {
+  //     // this.age;
+  //     console.log("fullName computed was called");
+  //     return `${this.lastName} ${
+  //       this.middleName
+  //     } ${this.firstName.toUpperCase()}`;
+  //   },
+  // },
+  // watch: {
+  //   age(newVal, oldVal) {
+  //     setTimeout(() => {
+  //       this.age = 20;
+  //     }, 2000);
+  //   },
+  // },
   // -----------binding classes exercise
   // data() {
   //   return {
@@ -66,7 +66,7 @@ const vm = Vue.createApp({
   // ------------list rendering----------------------
   // data() {
   //   return {
-  //     birds: ["pigeons", "eagles", "dove", "parrots"],
+  //     birds: ["pigeons", "eagles", "doves", "parrots"],
   //     people: [
   //       { name: "john", age: 20 },
   //       { name: "rick", age: 18 },
@@ -74,6 +74,32 @@ const vm = Vue.createApp({
   //     ],
   //   };
   // },
+  // -----------------Another lecture-----------------------------
+
+  data() {
+    return {
+      people: [
+        {
+          name: "john",
+          message: "hello world",
+        },
+        {
+          name: "rick",
+          message: "i like pie",
+        },
+        {
+          name: "amy",
+          message: "skydiving is fun!",
+        },
+      ],
+    };
+  },
+  methods: {
+    move() {
+      const first = this.people.shift();
+      this.people.push(first);
+    },
+  },
 }).mount(".app");
 
 // setTimeout(function () {
